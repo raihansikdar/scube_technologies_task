ada6f9f1fc530acfdf610fc30664437d62e421e5
Raihan Sikdar <raihansikdar10@gmail.com>
2024-02-19 20:23:47 +0600

    service has been added

---
+++ lib/services/network_caller.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart';
+import 'package:scube_technologies_task/services/network_response.dart';
+
+class NetworkCaller{
+  ///----------------------------------->> get request method <<----------------------------------
+  static Future<NetworkResponse> getRequest(String url,{bool isLogin = false}) async{
+    try{
+      Response response = await get(Uri.parse(url),headers: {'Content-Type': 'application/json'});
+
+      // log("getRequest statusCode ==> ${response.statusCode}");
+      //  log("getRequest body ==> ${response.body}");
+
+      if(response.statusCode == 200 ){
+        return NetworkResponse(isSuccess: true, statusCode: response.statusCode, body: jsonDecode(response.body));
+      }else{
+        return NetworkResponse(isSuccess: false, statusCode: response.statusCode, body: null);
+      }
+
+    }catch(e){
+      log(e.toString());
+    }
+    return NetworkResponse(isSuccess: false, statusCode: -1, body: null);
+  }
+
+  ///----------------------------------->> post request method <<----------------------------------
+
+  static Future<NetworkResponse> postRequest(String url,Map<String,dynamic>body) async{
+    try{
+      Response response = await post(Uri.parse(url),headers: {'Content-Type': 'application/json'}, body: jsonEncode(body));
+
+      log("postRequest statusCode ==> ${response.statusCode}");
+      log("postRequest body ==> ${response.body}");
+
+      if(response.statusCode == 200){
+        return NetworkResponse(isSuccess: true, statusCode: response.statusCode, body: jsonDecode(response.body));
+      }else{
+        return NetworkResponse(isSuccess: false, statusCode: response.statusCode, body: null);
+      }
+    }catch(e){
+      log(e.toString());
+    }
+    return NetworkResponse(isSuccess: false, statusCode: -1, body: null);
+  }
+
+  ///----------------------------------->> put request method <<----------------------------------
+  static Future<NetworkResponse> putRequest(String url, Map<String, dynamic> body) async {
+    try {
+      Response response = await put(Uri.parse(url), headers: {'Content-Type': 'application/json'}, body: jsonEncode(body));
+
+      if (response.statusCode == 200) {
+        return NetworkResponse(isSuccess: true, statusCode: response.statusCode, body: jsonDecode(response.body));
+      } else {
+        return NetworkResponse(isSuccess: false, statusCode: response.statusCode, body: null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return NetworkResponse(isSuccess: false, statusCode: -1, body: null);
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:scube_technologies_task/application/app.dart';
 
 import 'package:scube_technologies_task/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( ScubeTechnologiesTask());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
